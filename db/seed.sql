604021e4e43babc56ca7e58a5e6eaa3dd588647e
konojunya <junya03dance@gmail.com>
2018-04-13 21:02:22 +0900

    sample

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO user (student_id, password, name, sex) VALUES ("ohs50278", "konojunya", "河野 純也", "0");
+INSERT INTO user (student_id, password, name, sex) VALUES ("ohs50082", "hoge", "高鉾 大貴", "0");
+INSERT INTO user (student_id, password, name, sex) VALUES ("ohs50000", "huga", "佐々木 希", "1");
